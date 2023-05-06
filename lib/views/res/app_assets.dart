fd3b556576e6da82ed3c0a6bfa3b3db1d67b9ade
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-05-06 18:03:30 +0430

    Create app_assets.dart

---
+++ lib/views/res/app_assets.dart
@@ -0,0 +1,11 @@
+abstract class AppAssetsManagement {
+  static const String iconPath = 'assets/icons/';
+  static const String homeIcon = '${iconPath}home.svg';
+  static const String homeSelectedIcon = '${iconPath}home_selected.svg';
+  static const String exploreIcon = '${iconPath}explore.svg';
+  static const String exploreSelectedIcon = '${iconPath}explore_selected.svg';
+  static const String notificationsIcon = '${iconPath}notifications.svg';
+  static const String notificationsSelectedIcon =
+      '${iconPath}notifications_selected.svg';
+  static const String addPostIcon = '${iconPath}add_post.svg';
+}
