676e9f42332227c5cd1b9440f246cc116d028411
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-05-06 18:08:22 +0430

    Create add_post_screen.dart

---
+++ lib/views/screens/add_post/add_post_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddPostScreen extends StatelessWidget {
+  const AddPostScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('Add a Post Here')));
+  }
+}
