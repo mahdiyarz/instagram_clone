0c8973d9f3153b6059ab5d9bd016a625d9452563
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-05-08 00:49:18 +0430

    Create a button to navigate on second home screen

--- lib/views/screens/home/home_screen.dart
+++ lib/views/screens/home/home_screen.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/views/screens/home/home_pages/second_home_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Home'),
+    return SizedBox(
+      width: double.infinity,
+      height: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text('Home'),
+          ElevatedButton(
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const SecondHomeScreen(),
+              ),
+            ),
+            child: const Text('Next Page'),
+          ),
+        ],
+      ),
     );
   }
 }
