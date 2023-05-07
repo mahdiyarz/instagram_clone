35110e7938e7f45418b0f11613b7260c69472a06
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-05-08 00:49:54 +0430

    Implement complex onWillPop function and navigation history

--- lib/views/screens/tab/tab_screen.dart
+++ lib/views/screens/tab/tab_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -24,50 +26,83 @@ class _TabScreenState extends State<TabScreen> {
   final GlobalKey<NavigatorState> _notifyScreenKey = GlobalKey();
   final GlobalKey<NavigatorState> _profileScreenKey = GlobalKey();
 
+  late final navigationMapper = {
+    BottomNavIndex.homeIndex: _homeScreenKey,
+    BottomNavIndex.exploreIndex: _exploreScreenKey,
+    BottomNavIndex.notifyIndex: _notifyScreenKey,
+    BottomNavIndex.profileIndex: _profileScreenKey,
+  };
+
+  List<int> navigationHistory = [
+    0,
+  ];
+
+  Future<bool> _onWillPop() async {
+    if (navigationMapper[selectedIndex]!.currentState!.canPop()) {
+      navigationMapper[selectedIndex]!.currentState!.pop();
+    } else if (navigationHistory.length > 1) {
+      setState(() {
+        log('Remove the ${navigationHistory.last} from the list $navigationHistory');
+        navigationHistory.remove(navigationHistory.last);
+        log('Move to ${navigationHistory.last} from this list $navigationHistory');
+        selectedIndex = navigationHistory.last;
+      });
+    } else if (navigationHistory.length == 1) {
+      setState(() {
+        selectedIndex = navigationHistory.first;
+      });
+    }
+    log('Pressed back');
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-          child: Stack(
-        children: [
-          Positioned(
-            bottom: 60,
-            top: 0,
-            right: 0,
-            left: 0,
-            child: IndexedStack(
-              index: selectedIndex,
-              children: [
-                Navigator(
-                  key: _homeScreenKey,
-                  onGenerateRoute: (settings) => MaterialPageRoute(
-                    builder: (context) => const HomeScreen(),
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        body: SafeArea(
+            child: Stack(
+          children: [
+            Positioned(
+              bottom: 60,
+              top: 0,
+              right: 0,
+              left: 0,
+              child: IndexedStack(
+                index: selectedIndex,
+                children: [
+                  Navigator(
+                    key: _homeScreenKey,
+                    onGenerateRoute: (settings) => MaterialPageRoute(
+                      builder: (context) => const HomeScreen(),
+                    ),
                   ),
-                ),
-                Navigator(
-                  key: _exploreScreenKey,
-                  onGenerateRoute: (settings) => MaterialPageRoute(
-                    builder: (context) => const ExploreScreen(),
+                  Navigator(
+                    key: _exploreScreenKey,
+                    onGenerateRoute: (settings) => MaterialPageRoute(
+                      builder: (context) => const ExploreScreen(),
+                    ),
                   ),
-                ),
-                Navigator(
-                  key: _notifyScreenKey,
-                  onGenerateRoute: (settings) => MaterialPageRoute(
-                    builder: (context) => const NotifyScreen(),
+                  Navigator(
+                    key: _notifyScreenKey,
+                    onGenerateRoute: (settings) => MaterialPageRoute(
+                      builder: (context) => const NotifyScreen(),
+                    ),
                   ),
-                ),
-                Navigator(
-                  key: _profileScreenKey,
-                  onGenerateRoute: (settings) => MaterialPageRoute(
-                    builder: (context) => const ProfileScreen(),
+                  Navigator(
+                    key: _profileScreenKey,
+                    onGenerateRoute: (settings) => MaterialPageRoute(
+                      builder: (context) => const ProfileScreen(),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          navigationBottom(context),
-        ],
-      )),
+            navigationBottom(context),
+          ],
+        )),
+      ),
     );
   }
 
@@ -83,8 +118,17 @@ class _TabScreenState extends State<TabScreen> {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               IconButton(
-                onPressed: (() =>
-                    setState(() => selectedIndex = BottomNavIndex.homeIndex)),
+                onPressed: (() => setState(
+                      () => [
+                        selectedIndex = BottomNavIndex.homeIndex,
+                        if (!navigationHistory
+                            .contains(BottomNavIndex.homeIndex))
+                          {
+                            navigationHistory.add(BottomNavIndex.homeIndex),
+                            log('Add ${BottomNavIndex.homeIndex} to $navigationHistory'),
+                          }
+                      ],
+                    )),
                 icon: SvgPicture.asset(
                   selectedIndex == BottomNavIndex.homeIndex
                       ? AppAssetsManagement.homeSelectedIcon
@@ -93,7 +137,16 @@ class _TabScreenState extends State<TabScreen> {
               ),
               IconButton(
                 onPressed: (() => setState(
-                    () => selectedIndex = BottomNavIndex.exploreIndex)),
+                      () => [
+                        selectedIndex = BottomNavIndex.exploreIndex,
+                        if (!navigationHistory
+                            .contains(BottomNavIndex.exploreIndex))
+                          {
+                            navigationHistory.add(BottomNavIndex.exploreIndex),
+                            log('Add ${BottomNavIndex.exploreIndex} to $navigationHistory'),
+                          }
+                      ],
+                    )),
                 icon: SvgPicture.asset(
                     selectedIndex == BottomNavIndex.exploreIndex
                         ? AppAssetsManagement.exploreSelectedIcon
@@ -105,8 +158,17 @@ class _TabScreenState extends State<TabScreen> {
                 icon: SvgPicture.asset(AppAssetsManagement.addPostIcon),
               ),
               IconButton(
-                onPressed: (() =>
-                    setState(() => selectedIndex = BottomNavIndex.notifyIndex)),
+                onPressed: (() => setState(
+                      () => [
+                        selectedIndex = BottomNavIndex.notifyIndex,
+                        if (!navigationHistory
+                            .contains(BottomNavIndex.notifyIndex))
+                          {
+                            navigationHistory.add(BottomNavIndex.notifyIndex),
+                            log('Add ${BottomNavIndex.notifyIndex} to $navigationHistory'),
+                          }
+                      ],
+                    )),
                 icon: SvgPicture.asset(
                     selectedIndex == BottomNavIndex.notifyIndex
                         ? AppAssetsManagement.notificationsSelectedIcon
@@ -114,7 +176,16 @@ class _TabScreenState extends State<TabScreen> {
               ),
               IconButton(
                 onPressed: (() => setState(
-                    () => selectedIndex = BottomNavIndex.profileIndex)),
+                      () => [
+                        selectedIndex = BottomNavIndex.profileIndex,
+                        if (!navigationHistory
+                            .contains(BottomNavIndex.profileIndex))
+                          {
+                            navigationHistory.add(BottomNavIndex.profileIndex),
+                            log('Add ${BottomNavIndex.profileIndex} to $navigationHistory'),
+                          }
+                      ],
+                    )),
                 icon: selectedIndex == BottomNavIndex.profileIndex
                     ? const Icon(Icons.supervised_user_circle_rounded)
                     : const Icon(Icons.data_usage_rounded),
